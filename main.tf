d4fb3da670eb5db7910978f4d2a2b0af1c7d736a
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-29 00:59:46 +0100

    fix: update atom ref to fix alias record ttl/records conflict

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 # Molecule: Route53 Alias Record
 module "record" {
-  source = "git::https://github.com/PlatformStackPulse/tf-atom-route53-record-aws.git?ref=03611a6020bfac6dea77e0b158471cdab6411d69"
+  source = "git::https://github.com/PlatformStackPulse/tf-atom-route53-record-aws.git?ref=8ed4fda"
 
   context                = module.this.context
   zone_id                = var.zone_id
