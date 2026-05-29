e59dbf9e7bfe64377973107c57b3e021208ab8be
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-29 01:06:37 +0100

    fix: pass is_alias=true and update atom ref to e83e2ca

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 # Molecule: Route53 Alias Record
 module "record" {
-  source = "git::https://github.com/PlatformStackPulse/tf-atom-route53-record-aws.git?ref=8ed4fda"
+  source = "git::https://github.com/PlatformStackPulse/tf-atom-route53-record-aws.git?ref=e83e2ca"
 
   context                = module.this.context
   zone_id                = var.zone_id
   record_name            = var.record_name
   record_type            = "A"
+  is_alias               = true
   alias_name             = var.alias_name
   alias_zone_id          = var.alias_zone_id
   evaluate_target_health = var.evaluate_target_health
